664b0c5465543cc2b707ad3cdd6cb371ffea5d06
ratola137 <lucasratola101@gmail.com>
2026-01-25 19:04:06 -0300

    feat/04
    reordenação das pastas e comentários no código clusterizacao.py para melhor leitura

--- CASE 00 - PROJETO CLUSTERIZACAO DE CLIENTES/Clusterizacao.py
+++ CASE 00 - PROJETO CLUSTERIZACAO DE CLIENTES/Clusterizacao.py
@@ -4,17 +4,19 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-file_path = 'PYTHON/dados_clientes.csv'
+#define uma constante com o caminho do arquivo de dados
+file_path = 'CASE 00 - PROJETO CLUSTERIZACAO DE CLIENTES/dados_clientes.csv'
+#carrega o dataset
 data_set = pd.read_csv(file_path)
 
-#resumo estatístico dos dados
+#resumo estatístico dos dados (das 3 colunas que serão usadas na clusterização)
 data_set[['idade','renda_anual','pontuacao_gastos']].describe()
 
 #padronização dos dados - escala z-score dos dados
 padronizar = StandardScaler()
 dados_padronizados = padronizar.fit_transform(data_set[['idade','renda_anual','pontuacao_gastos']])
 
-#demonstração a via de comparação dos dados originais e padronizados
+#demonstração a via de comparação dos dados originais e padronizados - sem o índice e apenas as 10 primeiras linhas
 print(data_set[['idade','renda_anual','pontuacao_gastos']].head(10).to_string(index=False))
 print(dados_padronizados[:10])
 
@@ -25,6 +27,7 @@ kmeans = KMeans(n_clusters = k)
 #treina o modelo
 kmeans.fit(dados_padronizados)
 data_set['cluster'] = kmeans.labels_
+#mostra as 10 primeiras linhas do dataset com os clusters atribuídos
 print(data_set.head(10).to_string(index=False))
 #salva o novo arquivo com os segmentos
-data_set.to_csv('PYTHON/dados_clientes_segmentados.csv', index=False)
+data_set.to_csv('CASE 00 - PROJETO CLUSTERIZACAO DE CLIENTES/dados_clientes_segmentados.csv', index=False)
